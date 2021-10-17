84f67d6eaeb8521a32ccd40d523bfbdb0bce4156
宁鑫 <1819397331@qq.com>
2021-10-17 11:03:18 +0800

    init commit

--- book-codes/histogram.cu
+++ book-codes/histogram.cu
@@ -4,7 +4,7 @@
 #include <cassert>
 
 #define ARRAY_SIZE 1000000
-#define TOTAL_NUMBER 256
+#define TOTAL_NUMBER 64
 
 using namespace std;
 
@@ -25,7 +25,7 @@ __global__ void histogramKernel_0(unsigned char *d_hist_data, unsigned int *d_bi
 	}
 }
 
-__shared__ unsigned int d_bin_data_shared[256];
+__shared__ unsigned int d_bin_data_shared[TOTAL_NUMBER];
 /**
  * @brief histogram computing with shared memory
  */
@@ -33,12 +33,13 @@ __global__ void histogramKernel_1(unsigned char *d_hist_data, unsigned int *d_bi
 {
 	int thread_idx = blockIdx.x * blockDim.x + threadIdx.x;
 	d_bin_data_shared[threadIdx.x] = 0;
+	__syncthreads();
 	if (thread_idx < ARRAY_SIZE)
 	{
 		unsigned char value = d_hist_data[thread_idx];
 		atomicAdd(&(d_bin_data_shared[value]), 1);
 		__syncthreads();
-		atomicAdd(&(d_bin_data_shared[threadIdx.x]), d_bin_data_shared[threadIdx.x]);
+		atomicAdd(&(d_bin_data[threadIdx.x]), d_bin_data_shared[threadIdx.x]);
 	}
 }
 
@@ -56,9 +57,9 @@ int main()
 	unsigned int *result1;
 	cudaMallocManaged(&result1, sizeof(unsigned int) * TOTAL_NUMBER);
 	memset(result1, 0, sizeof(unsigned int) * TOTAL_NUMBER);
-
 	dim3 blockSize(TOTAL_NUMBER);
 	dim3 gridSize((blockSize.x + ARRAY_SIZE - 1) / blockSize.x);
+
 	double t1 = getCurrentTime();
 	histogramKernel_0<<<gridSize, blockSize>>>(d_data, result1);
 	cudaDeviceSynchronize();
@@ -69,17 +70,16 @@ int main()
 	cudaMallocManaged(&result2, sizeof(unsigned int) * TOTAL_NUMBER);
 	double t3 = getCurrentTime();
 	histogramKernel_1<<<gridSize, blockSize>>>(d_data, result2);
-	double t4 = getCurrentTime();
 	cudaDeviceSynchronize();
+	double t4 = getCurrentTime();
+
 	cout << "共享内存处理时间：" << t4 - t3 << " ms" << endl;
-	int sum = 0;
 	for (int i = 0; i < TOTAL_NUMBER; i++)
 	{
-		cout << result2[i] << endl;
-		sum += result2[i];
-		// assert(result1[i] == result2[i]);
+		// cout << (result1[i] == result2[i] ? 1 : i) << endl;
+		// sum += result2[i];
+		assert(result1[i] == result2[i]);
 	}
-	cout << sum << endl;
 	cudaFree(d_data);
 	return 0;
 }
